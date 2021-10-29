4f86cdf613da4e33df24ec8b4fe3411b51faa920
Jason <diurnalist@gmail.com>
2021-10-29 15:59:33 -0500

    Move to xena release
    Also removes the Jenkinsfiles as we are moving to GitHub actions.

--- build.py
+++ build.py
@@ -178,7 +178,7 @@ def cli(config_file=None, config_set=None, profile=None, pattern=None, build_dir
 
     # Kolla reads its input straight from sys.argv
     sys.argv = [""] + kolla_argv
-    bad, good, unmatched, skipped, unbuildable = kolla_build.run_build()
+    bad, good, unmatched, skipped, unbuildable, fail_allowed  = kolla_build.run_build()
     if bad:
         sys.exit(1)
 
